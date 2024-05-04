9e0817c72d96e2aa8f13aa784d23ce58d411657d
Irene-Busah <i.busah@alustudent.com>
2024-05-05 01:05:29 +0200

    set up the pages for the simple newsletter app

--- node-with-mongodb/newsletter-signup/app.js
+++ node-with-mongodb/newsletter-signup/app.js
@@ -1,13 +1,30 @@
 import express from "express";
 import bodyParser from "body-parser";
 import { request } from "express";
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const app = express();
 const PORT = 3000;
 app.use(bodyParser.urlencoded({ extended: true }));
 
+// static files configuration
+app.use(express.static("public"));
+
 app.get("/", (req, res) => {
-    res.send("<h1>Hello</h1>");
+    res.sendFile(__dirname + "/signup.html");
+})
+
+
+// post request for the signup page
+app.post("/", (req, res) => {
+    let lname = req.body.lname;
+    let fname = req.body.fname;
+    let email = req.body.email;
+    console.log(fname, lname, email);
 })
 
 
